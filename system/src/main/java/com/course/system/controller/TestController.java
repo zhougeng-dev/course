cdbdc0aa699bb5e0803f5c22b16135213fabd9ac
zhougeng-dev <1528015234@qq.com>
2021-01-13 13:52:53 +0800

    first

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.course.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    private  String test(){
+        return "success";
+    }
+}
