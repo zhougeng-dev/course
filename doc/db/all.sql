ebb0aa92681213cfbfb558504178099bc1e0531e
zhougeng-dev <1528015234@qq.com>
2021-02-23 17:14:32 +0800

    1.增加大章表chapter表，生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,10 +1,23 @@
+DROP TABLE IF EXISTS `chapter`;
+CREATE TABLE `chapter` (
+                           `id` char(8) NOT NULL,
+                           `course_id` char(8) DEFAULT NULL,
+                           `name` varchar(50) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 DROP TABLE IF EXISTS `test`;
-CREATE TABLE `test`
-(
-    `id`   varchar(10) NOT NULL,
-    `name` varchar(20) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+CREATE TABLE `test` (
+                        `id` varchar(10) NOT NULL,
+                        `name` varchar(20) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
 INSERT INTO `test` VALUES ('1', 'test');
 INSERT INTO `test` VALUES ('2', '测试');
 INSERT INTO `test` VALUES ('3', '测试');
+INSERT INTO `test` VALUES ('6', '测试6');
+INSERT INTO `test` VALUES ('9', '测试9');
